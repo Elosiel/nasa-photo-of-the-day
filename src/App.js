a4476fa1eac51fc38f2913cdb4bd664a2565eca8
elosiel guedes <elosiel@gmail.com>
2020-08-25 21:36:47 -0500

    New

--- src/App.js
+++ src/App.js
@@ -1,7 +1,15 @@
-import React from "react";
+import React, {useEffect} from "react";
 import "./App.css";
+import axios from 'axios';
 
 function App() {
+  useEffect(() => {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=DEMO_KEY&date=2020-08-25')
+    .then(response =>{
+      console.log(response.data)
+    })
+    .catch(err => console.log(err))
+  }, [])
   return (
     <div className="App">
       <p>
